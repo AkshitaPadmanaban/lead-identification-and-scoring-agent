952dcee68918bb353176a562c67bd056446ede7f
akshi <117973527+AkshitaPadmanaban@users.noreply.github.com>
2025-12-17 11:26:36 +0530

    Create domain_config.py

---
+++ config/domain_config.py
@@ -0,0 +1,23 @@
+TARGET_ROLES = [
+    "Director of Toxicology",
+    "Head of Preclinical Safety",
+    "Principal Scientist",
+    "Senior Scientist",
+    "Translational Toxicology Lead"
+]
+
+TARGET_KEYWORDS = [
+    "3D in vitro",
+    "organ-on-chip",
+    "in vitro toxicology",
+    "preclinical safety",
+    "drug induced toxicity",
+    "alternative to animal testing"
+]
+
+TARGET_INDUSTRIES = [
+    "Pharmaceuticals",
+    "Biotechnology",
+    "CRO",
+    "Academic Research"
+]
